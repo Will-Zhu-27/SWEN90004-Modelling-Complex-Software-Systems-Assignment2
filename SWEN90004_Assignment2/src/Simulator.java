215361d94012c6622e80a8b1d3dace9f9ef30a51
Will_Zhu <38975620+Will-Zhu-27@users.noreply.github.com>
2019-05-29 12:14:50 +1000

    update the README

--- SWEN90004_Assignment2/src/Simulator.java
+++ SWEN90004_Assignment2/src/Simulator.java
@@ -1,6 +1,15 @@
+/**
+ * The entrance of the simulator.
+ * @author yuqiangz@student.unimelb.edu.au
+ *
+ */
 public class Simulator {
 	public static final String OUTPUT_FILE_PATH = "output.csv";
+	
+	/**
+	 * @param args the incoming parameters
+	 */
 	public static void main(String[] args) {
-		DaisyWorld daisyWorld = new DaisyWorld(args);
+		new DaisyWorld(args);
 	}
 }
